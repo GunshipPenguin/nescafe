1198b737d860a2d280959d4d155539e45beca9c3
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-08-01 13:36:06 -0700

    Add Ppu register write methods to CpuMemory

--- src/CpuMemory.cs
+++ src/CpuMemory.cs
@@ -5,18 +5,44 @@ public class CpuMemory : Memory {
   byte[] internalRam = new byte[2048];
 
   Mapper mapper;
+  Console console;
 
   public CpuMemory(Console console) {
     mapper = new Nrom128Mapper(console.cartridge);
+    this.console = console;
   }
 
   public override void write(ushort address, byte data) {
-    if (address > 0x1FFF) {
-      throw new NotImplementedException();
+    if (address < 0x2000) { // Internal CPU RAM 
+      ushort addressIndex = handleInternalRamMirror(address);
+      internalRam[addressIndex] = data;
+    } else if (address < 0x1FF9) { // PPU Registers
+      writePpuRegister(address, data);
+    } else {
+      throw new NotImplementedException("Invalid CPU Memory Write to address: " + address.ToString("X4"));
     }
+  }
 
-    ushort addressIndex = handleInternalRamMirror(address);
-    internalRam[addressIndex] = data;
+  private void writePpuRegister(ushort address, byte data) {
+    ushort registerAddress = (byte) (0x2000 + ((address - 0x2000) % 8));
+    switch (registerAddress) {
+      case 0x2000: console.ppu.writePpuCtrl(data);
+        break;
+      case 0x2001: console.ppu.writePpuMask(data);
+        break;
+      case 0x2002: console.ppu.writePpuStatus(data);
+        break;
+      case 0x2003: console.ppu.writeOamAddr(data);
+        break;
+      case 0x2004: console.ppu.writeOamData(data);
+        break;
+      case 0x2005: console.ppu.writePpuScroll(data);
+        break;
+      case 0x2006: console.ppu.writePpuData(data);
+        break;
+      case 0x2007: console.ppu.writeOamDma(data);
+        break;
+    }
   }
 
   public override byte read(ushort address) {
--- src/Ppu.cs
+++ src/Ppu.cs
@@ -1,11 +1,43 @@
 using System;
 
 public class Ppu {
-    CpuMemory _cpuMemory;
-    PpuMemory _memory;
+  CpuMemory _cpuMemory;
+  PpuMemory _memory;
 
-    public Ppu(Console console) {
-        _cpuMemory = console.cpuMemory;
-        _memory = console.ppuMemory;
-    }
+  public Ppu(Console console) {
+    _cpuMemory = console.cpuMemory;
+    _memory = console.ppuMemory;
+  }
+
+  public void writePpuCtrl(byte data) {
+
+  }
+
+  public void writePpuMask(byte data) {
+
+  }
+
+  public void writePpuStatus(byte data) {
+
+  }
+
+  public void writeOamAddr(byte data) {
+
+  }
+
+  public void writeOamData(byte data) {
+
+  }
+
+  public void writePpuScroll(byte data) {
+
+  }
+
+  public void writePpuData(byte data) {
+
+  }
+
+  public void writeOamDma(byte data) {
+
+  }
 }
