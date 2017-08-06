dfb307fef1e6ead3ece6ecd1c34c478be731d0e5
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-08-06 00:38:46 -0700

    Add Palette RAM support to PpuMemory

--- src/PpuMemory.cs
+++ src/PpuMemory.cs
@@ -3,25 +3,41 @@ using System;
 public class PpuMemory : Memory {
   Console console;
   byte[] vRam;
+  byte[] paletteRam;
 
   public PpuMemory (Console console) {
     this.console = console;
     vRam = new byte[2048];
+    paletteRam = new byte[32];
+  }
+
+  public ushort getVramIndex(ushort address) {
+    return (ushort) (address - 0x2000);
+  }
+
+  public ushort getPaletteRamIndex(ushort address) {
+    return (ushort) (address - 0x3F00);
   }
 
   public override byte read(ushort address) {
+    byte data;
     if (address < 0x2000) { // CHR ROM pattern tables
-      return console.cartridge.readChrRom(address);
+      data = console.cartridge.readChrRom(address);
     } else if (address <= 0x2FFF) { // Internal vRam
-      return vRam[address - 0x2000];
+      data = vRam[getVramIndex(address)];
+    } else if (address >= 0x3F00 && address <= 0x3F1F) {
+      data = paletteRam[getPaletteRamIndex(address)];
     } else {
       throw new Exception("Invalid PPU Memory read at address: " + address.ToString("x4"));
     }
+    return data;
   }
 
   public override void write(ushort address, byte data) {
     if (address < 0x2FFF) { // Internal vRam
-      vRam[address] = data;
+      vRam[getVramIndex(address)] = data;
+    } else if (address >= 0x3F00 && address < 0x3F1F) {
+      paletteRam[getPaletteRamIndex(address)] = data;
     } else {
       throw new Exception("Invalid PPU Memory read at address: " + address.ToString("x4"));
     }
