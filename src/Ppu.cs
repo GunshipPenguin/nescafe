aafa1934104bcbda2af0c4627df3dba7ff4c7198
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-08-08 16:34:54 -0700

    Cache backgrond pattern, rename several vars/functions for clarity

--- src/Ppu.cs
+++ src/Ppu.cs
@@ -19,11 +19,16 @@ public class Ppu {
   int scanline;
   int cycle;
 
-  // Current nametable byte
-  byte nameTableByte;
-  byte attributeTableByte;
+  // Current nametable, attribute table and pattern bytes
+  byte currNameTableByte;
+  byte currAttributeTableByte;
+  byte[] currBgPattern; // 2 bytes
 
+  // Base nametable and pattern table address
   ushort baseNameTableAddr;
+  ushort bgPatternTableAddress;
+
+  // Vram increment per write to PPUDATA
   int vRamIncrement;
 
   // Last value written to a PPU register
@@ -62,6 +67,8 @@ public class Ppu {
     _console = console;
     bitmapData = new byte[256 * 240];
 
+    currBgPattern = new byte[2];
+
     expectingPpuAddrLo = false;
 
     scanline = 0;
@@ -70,7 +77,7 @@ public class Ppu {
     oam = new byte[256];
   }
 
-  byte getBackgroundColor(int paletteNum, int colorNum) {
+  byte lookupBackgroundColor(int paletteNum, int colorNum) {
     // Special case for universal background color
     if (colorNum == 0) {
       return _memory.read(0x3F00);
@@ -117,29 +124,21 @@ public class Ppu {
   }
 
   // Gets the CHR of the current background pixel as specified in nametablebyte
-  private byte fetchBackgroundPattern(int x, int y) {
+  private byte getBackgroundColorAtCoords(int x, int y) {
     // X and Y pixel coordinates relative to the pattern in the pattern table
     int patternX = x % 8;
     int patternY = y % 8;
 
-    // Load base pattern table address, get pattern address
-    ushort patternTableBaseAddress = (ushort) (flagBgPatternTableAddr == 0 ? 0x0000 : 0x1000);
-    ushort patternAddress = (ushort) (patternTableBaseAddress + nameTableByte);
-
-    // Fetch low and high byte of the pixel in the pattern
-    byte loByte = _memory.read((ushort) (patternAddress + patternY));
-    byte hiByte = _memory.read((ushort) (patternAddress + patternY + 8));
-
-    // Create color number
-    byte loBit = (byte) (loByte >> patternX);
-    byte hiBit = (byte) (hiByte >> patternX);
+    // Create color number from bitfields in current background pattern
+    byte loBit = (byte) (currBgPattern[0] >> patternX);
+    byte hiBit = (byte) (currBgPattern[1] >> patternX);
     byte colorNum = (byte) (((hiBit << 1) | loBit) & 0x03);
 
     // Grab palette data from attribute table
-    byte attributeBits = getAttributeBitsFromCoords(x, y, attributeTableByte);
+    byte attributeBits = getAttributeBitsFromCoords(x, y, currAttributeTableByte);
 
     // Lookup and return color
-    byte color = getBackgroundColor(attributeBits, colorNum);
+    byte color = lookupBackgroundColor(attributeBits, colorNum);
     return color;
   }
 
@@ -147,11 +146,11 @@ public class Ppu {
     int pixelX = cycle - 1;
     int pixelY = scanline - 1;
 
-    byte color = fetchBackgroundPattern(pixelX, pixelY);
+    byte color = getBackgroundColorAtCoords(pixelX, pixelY);
     bitmapData[pixelY * 256 + pixelX] = color;
   }
 
-  private void fetchNameTableByte() {
+  private void updateNameTableByte() {
     int x = cycle - 1;
     int y = scanline - 1;
 
@@ -160,10 +159,14 @@ public class Ppu {
     int tileY = y / 8;
 
     ushort currNameTableAddr = (ushort) (baseNameTableAddr + (ushort) ((240 * tileY + tileX) / 8));
-    nameTableByte = _memory.read(currNameTableAddr);
+    currNameTableByte = _memory.read(currNameTableAddr);
+
+    ushort patternAddress = (ushort) (bgPatternTableAddress + currNameTableByte);
+    currBgPattern[0] = _memory.read(patternAddress);
+    currBgPattern[1] = _memory.read((ushort) (patternAddress + 1));
   }
 
-  void fetchAttributeTableByte() {
+  void updateAttributeTableByte() {
     int x = cycle - 1;
     int y = scanline - 1;
 
@@ -172,13 +175,10 @@ public class Ppu {
     int blockY = y / 32;
 
     int attributeByteIndex = (blockY * 8) + blockX;
-    byte attributeTableByte = _memory.read((ushort) (baseNameTableAddr + 960 + attributeByteIndex)); // Attribute tables are 960 bytes from start of nametable
+    byte currAttributeTableByte = _memory.read((ushort) (baseNameTableAddr + 960 + attributeByteIndex)); // Attribute tables are 960 bytes from start of nametable
   }
 
   public void step() {
-    // System.Console.Write("scanline:" + scanline.ToString() + " ");
-    // System.Console.Write("cycle:" + cycle.ToString());
-    // System.Console.Write("\n");
     if (scanline == 0) {  // Do nothing, dummy scanline
 
     } else if (scanline >= 1 && scanline <= 240) { // Rendering scanlines
@@ -186,10 +186,10 @@ public class Ppu {
 
       } else if (cycle >= 1 && cycle <= 256) {
         if (cycle % 8 == 0) { // Fetch new rendering information if needed
-          fetchNameTableByte();
+          updateNameTableByte();
         }
         if (cycle % 32 == 0) {
-          fetchAttributeTableByte();
+          updateAttributeTableByte();
         }
         
         renderPixel();
@@ -277,6 +277,7 @@ public class Ppu {
     // Set values based off flags
     baseNameTableAddr = (ushort) (0x2000 + 0x4000*flagBaseNameTableAddr);
     vRamIncrement = (flagVRamIncrement == 0) ? 1 : 32;
+    bgPatternTableAddress = (ushort) (flagBgPatternTableAddr == 0 ? 0x0000 : 0x1000);
   }
 
   void writePpuMask(byte data) {
