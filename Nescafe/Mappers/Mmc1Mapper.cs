8e9ec10a6a84d3222ccf89fe2dcf364aab3099a5
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-08-29 16:23:36 -0700

    Document Mmc1Mapper.cs

--- Nescafe/Mappers/Mmc1Mapper.cs
+++ Nescafe/Mappers/Mmc1Mapper.cs
@@ -2,6 +2,9 @@
 
 namespace Nescafe.Mappers
 {
+    /// <summary>
+    /// Represents Nintendo's MMC1 mapper.
+    /// </summary>
     public class Mmc1Mapper : Mapper
     {
         // Common shift register
@@ -31,6 +34,10 @@ namespace Nescafe.Mappers
         // Current number of writes to internal shift register
         int _shiftCount;
 
+        /// <summary>
+        /// Construct a new MMC1 mapper.
+        /// </summary>
+        /// <param name="console">the console that this mapper is a part of</param>
         public Mmc1Mapper(Console console)
         {
             _console = console;
@@ -48,6 +55,11 @@ namespace Nescafe.Mappers
             _vramMirroringType = VramMirroring.Horizontal;
         }
 
+        /// <summary>
+        /// Read a byte from the specified address.
+        /// </summary>
+        /// <returns>the byte read from the specified address</returns>
+        /// <param name="address">the address to read a byte from</param>
         public override byte Read(ushort address)
         {
             byte data;
@@ -76,6 +88,12 @@ namespace Nescafe.Mappers
             return data;
         }
 
+        /// <summary>
+        /// Read a byte from the specified address.
+        /// </summary>
+        /// <returns>the byte read from the specified address</returns>
+        /// <param name="address">the address to read a byte from</param>
+        /// <param name="data">The data to write to the address</param>
         public override void Write(ushort address, byte data)
         {
             if (address < 0x2000)
