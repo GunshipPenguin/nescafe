9bd85ea2162c361ad5c23ff22d41dbbf85d289d2
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-08-29 16:20:53 -0700

    Document NromMapper.cs

--- Nescafe/Mappers/NromMapper.cs
+++ Nescafe/Mappers/NromMapper.cs
@@ -2,8 +2,15 @@
 
 namespace Nescafe.Mappers
 {
+    /// <summary>
+    /// Represents Nintendo's NROM Mapper.
+    /// </summary>
     class NromMapper : Mapper
     {
+        /// <summary>
+        /// Construct a new NROM mapper.
+        /// </summary>
+        /// <param name="console">Console.</param>
         public NromMapper(Console console)
         {
             _console = console;
@@ -16,22 +23,34 @@ namespace Nescafe.Mappers
             return _console.Cartridge.PrgRomBanks == 1 ? (ushort)(mappedAddress % 16384) : mappedAddress; // Wrap if only 1 PRG bank
         }
 
+        /// <summary>
+        /// Read a byte from the specified address.
+        /// </summary>
+        /// <returns>the byte read from the specified address</returns>
+        /// <param name="address">the address to read a byte from</param>
         public override byte Read(ushort address)
         {
+            byte data;
             if (address < 0x2000) // CHR rom stored from $0000 to $1FFF
             {
-                return _console.Cartridge.ReadChr(address);
+                data = _console.Cartridge.ReadChr(address);
             }
             else if (address >= 0x8000) // PRG ROM stored at $8000 and above
             {
-                return _console.Cartridge.ReadPrgRom(AddressToPrgRomIndex(address));
+                data = _console.Cartridge.ReadPrgRom(AddressToPrgRomIndex(address));
             }
             else
             {
                 throw new Exception("Invalid mapper read");
             }
+            return data;
         }
 
+        /// <summary>
+        /// Writes a byte to the specified address.
+        /// </summary>
+        /// <param name="address">the address to write a byte to</param>
+        /// <param name="data">the byte to write to the address</param>
         public override void Write(ushort address, byte data)
         {
             if (address < 0x2000) // CHR RAM
